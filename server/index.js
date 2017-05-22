71f53097c848140ebd2b2f8c6051fec20e4630c4
grantran <grant.tran@gmail.com>
2017-05-22 03:03:05 +0000

    minor code style corrections

--- server/index.js
+++ server/index.js
@@ -20,9 +20,9 @@ MongoClient.connect(MONGODB_URI, (err, db) => {
   }
   console.log(`Connected to mongodb: ${MONGODB_URI}`);
 
-
   const DataHelpers = require("./lib/data-helpers.js")(db);
   const tweetsRoutes = require("./routes/tweets")(DataHelpers);
+  
   app.use("/tweets", tweetsRoutes);
 });
 
